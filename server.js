10d17ec5b86f073f94030d471f8a6b9d826dfbb3
Dahmani-i <Ismail.dahmani2000@gmail.com>
2025-06-15 22:24:07 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const app = express();
+app.use(cors({ origin: '*' }));
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
 
