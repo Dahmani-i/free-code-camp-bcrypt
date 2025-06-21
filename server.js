80996134a84b25b82841e2a4241e62bacfabcd0a
Dahmani-i <Ismail.dahmani2000@gmail.com>
2025-06-21 18:51:51 +0000

    Hash_and_Compare__Passwords_Asynchronously_done

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
 const bcrypt = require('bcrypt');
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res)
+    });
+    /*Store hash in your db*/
+    return hash
+});
 
 
 //END_ASYNC
